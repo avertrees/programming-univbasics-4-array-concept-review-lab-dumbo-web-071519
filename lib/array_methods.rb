90f38aed43e754631861898a5e24205b5e29a1ca
captivating-form-0787 <avertrees447@gmail.com>
2019-06-14 14:58:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,13 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
+  i, max = 0
   # Add your solution here
 end
 
 def find_min_value(array)
+  i = log
+  min = 1000
   # Add your solution here
 end
+
