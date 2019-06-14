6bf4b45bc275e87084398a37f9605d032a3a6347
captivating-form-0787 <avertrees447@gmail.com>
2019-06-14 15:20:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,6 +4,8 @@ def find_element_index(array, value_to_find)
   found = false
   while i < array.length do
     if array[i] == value_to_find
+      puts array[i]
+      puts value_to_find
       found = true
       return i
     end
